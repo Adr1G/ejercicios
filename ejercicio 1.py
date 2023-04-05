23e3686a5f4457171679b12d94cb7b21565a0b6a
Adr1G <129990647+Adr1G@users.noreply.github.com>
2023-04-05 14:53:30 -0400

    Add files via upload

---
+++ ejercicio 1.py
@@ -0,0 +1,15 @@
+radio1 = 3
+radio2 = 8
+radio3 = 10
+
+pi = 3.141593 #definir la constante pi con 6 decimales
+ 
+#calcular la circunferencia
+circunferencia1 = round(2 * pi * radio1, 6)
+circunferencia2 = round(2 * pi * radio2, 6)
+circunferencia3 = round(2 * pi * radio3, 6)
+
+#imprimir los resultados 
+print("La circunferencia con radio", radio1, "es:", circunferencia1)
+print("La circunferencia con radio", radio2, "es:", circunferencia2)
+print("La circunferencia con radio", radio3, "es:", circunferencia3)
